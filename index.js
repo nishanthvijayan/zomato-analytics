09910b4d0ee5c5ff6d67089269aa473cca9b83ce
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2019-01-26 18:18:35 +0530

    Refactor

--- index.js
+++ index.js
@@ -7,20 +7,13 @@ const {
 } = require('./orders.js');
 const scrapeZomatoOrders = require('./zomato');
 
+const SECTION_BREAK = '\n\n';
+
 const ORDERS_OUTPUT_FILE = 'orders.json';
 const readOrdersFromFile = () => JSON.parse(fs.readFileSync(ORDERS_OUTPUT_FILE));
-
-const parseCLIArgs = () => {
-  cli
-    .version('0.1.0')
-    .option('-i, --input [file]', 'Read order from file')
-    .option('-o, --output [file]', 'Write the extracted orders to file')
-    .parse(process.argv);
-};
+const writeOrdersToFile = orders => fs.writeFileSync(ORDERS_OUTPUT_FILE, JSON.stringify(orders));
 
 async function main() {
-  parseCLIArgs();
-
   let orders;
   if (cli.input) {
     orders = readOrdersFromFile();
@@ -28,21 +21,34 @@ async function main() {
     orders = await scrapeZomatoOrders();
 
     if (cli.output) {
-      fs.writeFileSync(ORDERS_OUTPUT_FILE, JSON.stringify(orders));
+      writeOrdersToFile(orders);
     }
   }
 
   const isOrderDelivered = ({ status }) => status === 'Delivered';
   const deliveredOrders = orders.filter(isOrderDelivered);
+  console.log(`Your total delivered orders are: ${deliveredOrders.length}`);
 
-  console.log('Top Restaurants\n');
+  console.log(SECTION_BREAK);
+
+  console.log("Top 10 Restaurants from where you've ordered:\n");
   printOrderByRestaurantsGraph(deliveredOrders, 10);
 
-  console.log('\n\nLast 24 Months\n');
-  printOrdersOfLastMonthsGraph(deliveredOrders, 24);
+  console.log(SECTION_BREAK);
 
-  console.log('\n\nDays\n');
+  console.log('Distribution of your spendings over the last 12 Months\n');
+  printOrdersOfLastMonthsGraph(deliveredOrders, 12);
+
+  console.log(SECTION_BREAK);
+
+  console.log('Weekday wise distribution of your spendings\n');
   printOrdersByDayGraph(deliveredOrders);
 }
+
+cli
+  .version('0.1.0')
+  .option('-i, --input [file]', 'Read order from file')
+  .option('-o, --output [file]', 'Write the extracted orders to file')
+  .parse(process.argv);
 
 main();
